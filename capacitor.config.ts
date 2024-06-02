6eddf130e812aaecf9deeb8d8fa134c11d49b56e
Ane <ane.carvalhoss@gmail.com>
2024-06-02 11:29:09 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'meu-projeto-teste',
+  webDir: 'www'
+};
+
+export default config;
